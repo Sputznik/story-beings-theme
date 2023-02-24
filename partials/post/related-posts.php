e8252593d9b5ba900c3d502f89a6e49941d556e1
Guneet Narula <guneetnarula@sputznik.com>
2023-02-24 17:17:38 +0530

    Update related-posts.php
    Increased posts per page

--- partials/post/related-posts.php
+++ partials/post/related-posts.php
@@ -6,10 +6,10 @@ $post_id = get_the_ID();
 $tags = wp_get_post_tags( $post_id, array( 'fields' => 'slugs' ) );
 $tags_str = implode( ',', $tags );
 
-$shortcode_str = do_shortcode("[orbit_query posts_per_page='6' style='related-posts-slider' tag='".$tags_str."' post__not_in='".$post_id."' ]");
+$shortcode_str = do_shortcode("[orbit_query posts_per_page='9' style='related-posts-slider' tag='".$tags_str."' post__not_in='".$post_id."' ]");
 if( $tags_str && strlen( $shortcode_str ) > 0 ):?>
   <div class="related-posts">
-    <h3 class="headline sbct-dec-af">Recent Stories <i class="fa fa-angle-right" aria-hidden="true"></i></h3>
+    <h3 class="headline sbct-dec-af">Related Stories <i class="fa fa-angle-right" aria-hidden="true"></i></h3>
     <?php echo $shortcode_str;?>
   </div>
 <?php endif;?>
